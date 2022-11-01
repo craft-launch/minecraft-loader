a3194a75acaf9fb0f073a7d91ffeb603c47837a2
andre <61792917+luuxis@users.noreply.github.com>
2022-11-02 00:17:03 +0100

    fix path 1.13.2 > 1.19.x

--- utils/utils.js
+++ utils/utils.js
@@ -30,7 +30,7 @@ async function getPathLibraries(main, nativeString, forceExt) {
     let libSplit = main.split(':')
     let fileName = libSplit[3] ? `${libSplit[2]}-${libSplit[3]}` : libSplit[2];
     let finalFileName = fileName.includes('@') ? fileName.replace('@', '.') : `${fileName}${nativeString || ''}${forceExt || '.jar'}`;
-    let pathLib = `${libSplit[0].replace(/\./g, '/')}/${libSplit[1]}/${libSplit[2]}`
+    let pathLib = `${libSplit[0].replace(/\./g, '/')}/${libSplit[1]}/${libSplit[2].split('@')[0]}`
     return {
         path: pathLib,
         name: `${libSplit[1]}-${finalFileName}`
